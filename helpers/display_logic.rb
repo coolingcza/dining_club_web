f374e3f9fedc25814a3699bdd8a6fd0916c12fb9
Crystal Cooling <cooling.cne@gmail.com>
2015-02-13 15:44:13 -0600

    put 'helper' methods back in main.rb, removed helper module declaration. Kept display_logic file for future reference.

--- helpers/display_logic.rb
+++ helpers/display_logic.rb
@@ -7,8 +7,8 @@
 # #existing_club_display
 # #edited_club_display
 # #edit_actions
-
-module DisplayLogic
+#
+# module DisplayLogic
 
   # Public: new_club_display
    # Inserts new club and members into database, assigns
@@ -23,19 +23,19 @@ module DisplayLogic
    # State changes:
    # Assigns @club, @memberlist, @vartext.
 
-  def new_club_display
-    @club = DinnerClub.new({"name" => params["clubname"]})
-    @club.insert
-    @members = params["members"].split", "
-    @memberlist = []
-    @members.each do |m|
-      newperson = Person.new({"name"=>m,"club_id"=>@club.id})
-      newperson.insert
-      @memberlist << newperson
-    end
-    @vartext = "New Club Created:"
-  
-  end
+  # def new_club_display
+    # @club = DinnerClub.new({"name" => params["clubname"]})
+    # @club.insert
+    # @members = params["members"].split", "
+    # @memberlist = []
+    # @members.each do |m|
+    #   newperson = Person.new({"name"=>m,"club_id"=>@club.id})
+    #   newperson.insert
+    #   @memberlist << newperson
+    # end
+    # @vartext = "New Club Created:"
+    #
+  # end
 
   # Public: existing_club_display
    # Assigns route variables for display of existing club info.
@@ -49,11 +49,11 @@ module DisplayLogic
    # State changes:
    # Assigns @club, @memberlist, @vartext.
 
-  def existing_club_display
-    @club = DinnerClub.find("dinnerclubs",params["clubid"])
-    @memberlist = Person.where_club_id(@club.id)
-    @vartext = "Existing Club"
-  end
+  # def existing_club_display
+  #   @club = DinnerClub.find("dinnerclubs",params["clubid"])
+  #   @memberlist = Person.where_club_id(@club.id)
+  #   @vartext = "Existing Club"
+  # end
 
   # Public: edited_club_display
    # Calls #edit_actions and assigns route variables for display of 
@@ -68,12 +68,25 @@ module DisplayLogic
    # State changes:
    # Assigns @club, @memberlist, @vartext.
 
-  def edited_club_display
-    @club = DinnerClub.find("dinnerclubs",params["clubid"])
-    edit_actions
-    @memberlist = Person.where_club_id(@club.id)
-    @vartext = "Modified Club Info"
-  end
+  # def edited_club_display
+    # @club = DinnerClub.find("dinnerclubs",params["clubid"])
+    # if params["newclubname"]
+    #   @club.name = params["newclubname"]
+    #   @club.save
+    # elsif params["oldmemname"]
+    #   @member = Person.where_name(params["oldmemname"])[0]
+    #   @member.name = params["newmemname"]
+    #   @member.save
+    # elsif params["addmemname"]
+    #   @member = Person.new({"name"=>params["addmemname"],"club_id"=>@club.id})
+    #   @member.insert
+    # elsif params["remmemname"]
+    #   @member = Person.where_name(params["remmemname"])
+    #   @member[0].delete
+    # end
+    # @memberlist = Person.where_club_id(@club.id)
+    # @vartext = "Modified Club Info"
+  # end
 
   # Public: edit_actions
    # Alters @club or @member object and saves changes to database.
@@ -87,21 +100,21 @@ module DisplayLogic
    # State changes:
    # Modifies database.
 
-  def edit_actions
-    if params["newclubname"]
-      @club.name = params["newclubname"]
-      @club.save
-    elsif params["oldmemname"]
-      @member = Person.where_name(params["oldmemname"])[0]
-      @member.name = params["newmemname"]
-      @member.save
-    elsif params["addmemname"]
-      @member = Person.new({"name"=>params["addmemname"],"club_id"=>@club.id})
-      @member.insert
-    elsif params["remmemname"]
-      @member = Person.where_name(params["remmemname"])
-      @member[0].delete
-    end
-  end
+  # def edit_actions
+  #   if params["newclubname"]
+  #     @club.name = params["newclubname"]
+  #     @club.save
+  #   elsif params["oldmemname"]
+  #     @member = Person.where_name(params["oldmemname"])[0]
+  #     @member.name = params["newmemname"]
+  #     @member.save
+  #   elsif params["addmemname"]
+  #     @member = Person.new({"name"=>params["addmemname"],"club_id"=>@club.id})
+  #     @member.insert
+  #   elsif params["remmemname"]
+  #     @member = Person.where_name(params["remmemname"])
+  #     @member[0].delete
+  #   end
+  # end
 
-end
+# end
--- main.rb
+++ main.rb
@@ -12,9 +12,6 @@ require_relative "helpers/display_logic"
 
 
 
-helpers DisplayLogic
-
-
 
 get "/" do
   erb :welcome, :layout => :boilerplate
@@ -27,12 +24,49 @@ end
 
 get "/display_club_info" do
   @source = request.referer
+  
   if @source.include? "create_new_form"
-    new_club_display
+    @club = DinnerClub.new({"name" => params["clubname"]})
+    @club.insert
+    members = params["members"].split", "
+    @memberlist = []
+    members.each do |m|
+      newperson = Person.new({"name"=>m,"club_id"=>@club.id})
+      newperson.insert
+      @memberlist << newperson
+    end
+    @vartext = "New Club Created:"
+    
   elsif @source.include? "existing"
-    existing_club_display
+    @club = DinnerClub.find("dinnerclubs",params["clubid"])
+    @memberlist = Person.where_club_id(@club.id)
+    @vartext = "Existing Club"
+    
   else #@source includes edit_options
-    edited_club_display
+    @club = DinnerClub.find("dinnerclubs",params["clubid"])
+    
+    if params["newclubname"]
+      @club.name = params["newclubname"]
+      @club.save
+      
+    elsif params["oldmemname"]
+      @member = Person.where_name(params["oldmemname"])[0]
+      @member.name = params["newmemname"]
+      @member.save
+      
+    elsif params["addmemname"]
+      @member = Person.new({"name"=>params["addmemname"],"club_id"=>@club.id})
+      @member.insert
+      
+    elsif params["remmemname"]
+      @member = Person.where_name(params["remmemname"])
+      @member[0].delete
+      
+    end
+    
+    @memberlist = Person.where_club_id(@club.id)
+    @vartext = "Modified Club Info"
+    
   end
 
   erb :display_club_info, :layout => :boilerplate
